fe337e6f6979f4ef9c9b92ce47b34c35b07ab1db
jerobinjacob <jerobinjacob@gmail.com>
2025-05-31 16:53:49 +0530

    Removed Classes and Objects Folder

--- Classes and Objects/coex2.py
+++
@@ -1,6 +0,0 @@
-class fruit():
-    def __init__(self, color):
-        self.color= color
-
-apple = fruit("Red")
-print(apple.color)
--- Classes and Objects/comprehensions.py
+++
@@ -1,5 +0,0 @@
-list_squares = [x * x for x in range(5)]
-print(list_squares)
-
-dict_squares = {x : x * x for x in range(5)}
-print(dict_squares)
